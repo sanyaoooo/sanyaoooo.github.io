7c09b84838323c439835b3a4dbda2eca9a58f7e3
sanyaoooo <sanyaobox@gmail.com>
2021-08-11 15:12:34 +0800

    Add ajex

--- demo/wagikg-test/js/main.js
+++ demo/wagikg-test/js/main.js
@@ -1,3 +1,15 @@
 $.ajax({
-  
+  type: 'POST',
+  url: 'http://125.228.2.127/interview/RtrS0811/list.php' ,
+  data: {
+    "action" : "GetList",
+    "table" : "testproductdata"
+  } ,
+  dataType: 'json',
+  success: function(data) { 
+    console.log(data);
+  }, 
+  error : function() { 
+    console.log('error'); 
+  } 
 });
